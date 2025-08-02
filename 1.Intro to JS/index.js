a419d2815f69a43eee802b38bb285f8616abf22c
Jyotsna2029 <chauhanjyotsna049@gmail.com>
2025-08-02 14:03:32 +0530

    Add files via upload

---
+++ 1.Intro to JS/index.js
@@ -0,0 +1,13 @@
+console.log("Hello Coder Army");
+let num = 10;
+console.log(num);
+let naMe = "Jyotsna";
+num = 100;
+console.log(naMe);
+console.log(num);
+
+// constant
+const constantVal = 123456789;
+console.log(constantVal);
+// constantVal = 4321;   //ERROR TypeError: Assignment to constant variable.
+// console.log(constantVal); 
---
+++ 2.Part-1/first.js
@@ -0,0 +1,32 @@
+
+// num
+let num = 20;
+console.log(num);
+console.log(typeof num);
+
+// string
+let str = "Jyotsna";
+console.log(str);
+console.log(typeof str);
+
+//BOOLEAN
+let statement = true;
+console.log(statement);
+console.log(typeof statement);
+
+// bit int
+let bigInteger = 98765432123456789876543212345678987654321n;
+console.log(bigInteger);
+console.log(typeof bigInteger);
+  
+
+// undefined
+let undef = undefined;
+console.log(undef);
+console.log(typeof undef);
+
+
+// NULL
+let x = null;
+console.log(x);
+console.log(typeof x);
