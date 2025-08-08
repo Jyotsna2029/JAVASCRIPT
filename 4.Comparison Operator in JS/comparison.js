fb97ddb9af41e3ed7108591f71760dda71a73b11
Jyotsna2029 <chauhanjyotsna049@gmail.com>
2025-08-08 22:23:02 +0530

    Add files via upload

---
+++ 4.Comparison Operator in JS/comparison.js
@@ -0,0 +1,17 @@
+
+
+let a1 = 1;
+let a2 = 2;
+// comparison operator
+console.log(a1==a2);
+console.log(a1>a2);  
+console.log(a1<a2);
+console.log(a1>=a2);
+console.log(a1<=a2);
+
+
+let num = 10;
+let str = "10";
+console.log("num==str");
+console.log(num == str);
+
