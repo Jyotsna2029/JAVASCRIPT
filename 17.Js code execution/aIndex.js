428548f9eaa5481d2d8c70af1aff9597d961b703
Jyotsna2029 <chauhanjyotsna049@gmail.com>
2025-08-27 23:24:34 +0530

    Add files via upload

---
+++ 17.Js code execution/aIndex.js
@@ -0,0 +1,8 @@
+console.log(b);
+let x = 10; 
+
+// Js is a synchronous single threaded language.
+
+// Hoisting in JavaScript is the behavior where variable and function declarations are moved to the top of their containing scope during the compilation phase, before the code has been executed.
+
+// This means that JavaScript "hoists" or lifts the declarations (not the assignments) to the top, so you can refer to them before they are written in the code. However, only the declarations are hoisted, not the assignments or initializations.
