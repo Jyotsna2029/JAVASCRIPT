dcc8a5bb4a31c877ce4b30a3e9a8f66caaa40969
Jyotsna2029 <chauhanjyotsna049@gmail.com>
2025-08-30 00:30:33 +0530

    Add files via upload

---
+++ 19.Intro to DOM/aScript.js
@@ -0,0 +1,13 @@
+
+
+// class in JS is className:
+const obj = document.getElementById("first");
+obj.className = "heading";
+
+// get element by id
+document.getElementById('first').innerHTML = "NAMASTE SABHI KO!";
+
+// get element by class Name
+const objClass = document.getElementsByClassName("header3");
+console.log(objClass[0]);
+// objClass[0].style.fontSize = "10px";
